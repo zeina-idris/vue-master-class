a186d3d98ca9e54b6bd66672a31916a719fa35fc
Zeina Idris <idris.zeina@gmail.com>
2018-05-14 11:41:19 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <div class="container">
+      <router-view/>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+@import "assets/css/stylesheet.css"
+</style>
